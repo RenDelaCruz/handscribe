14277578638c7b7815d8d1d09634a4ce86430b19
Ren Dela Cruz <ren_delacruz@outlook.com>
2023-04-05 19:17:43 -0400

    Added unit tests for margin sucessiveletter class

---
+++ tests/test_dataclass.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from src.dataclass import SuccessiveLetter
+
+
+class SuccessiveLetterTests(TestCase):
+    def setUp(self) -> None:
+        self.centre_x = 50
+        self.width = 20
+        self.successive_letter = SuccessiveLetter(centre_x=50, width=20)
+
+    def test__returns_left_margin(self) -> None:
+        # when
+        result = self.successive_letter.left_margin
+
+        # then
+        self.assertEqual(result, self.centre_x - self.width)
+
+    def test__returns_right_margin(self) -> None:
+        # when
+        result = self.successive_letter.left_margin
+
+        # then
+        self.assertEqual(result, self.centre_x + self.width)
